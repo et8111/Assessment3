e759b1ba4bf3f6e332664d6b40c45d15405ab333
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-26 15:12:08 -0400

    One added

---
+++ MyPeeps/MyPeeps/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPeeps
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool isAdult { get; set; }
+
+        public Person(string first, string last, int age, string email)
+        {
+            FirstName = first;
+            LastName = last;
+            Age = age;
+            Email = email;
+        }
+
+        public bool getIsAdult()
+        {
+            if (Age > 18)
+                isAdult = true;
+            else
+            {
+                isAdult = false;
+            }
+            return isAdult;
+        }
+
+    }
+}
---
+++ MyPeeps/MyPeeps/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPeeps
+{
+    class Program
+    {
+        static void NEWPERSON(List<Person> person)
+        {
+            Console.WriteLine("Enter the following info...");
+            Console.Write("Age: ");
+            string tempAge = Console.ReadLine();
+
+            if (int.Parse(tempAge) <= 18)
+                Console.WriteLine("Whos CHAI OLD IS THIS?!?!?");
+            else
+            {
+                string tempF = "", tempL = "", tempE = "";
+                ask1:
+                Console.Write("First Name: ");
+                tempF = Console.ReadLine();
+                if (tempF.Contains(" "))
+                {
+                    Console.WriteLine("ONE NAME ONLY");
+                    goto ask1;
+                }
+                ask2:
+                Console.Write("Last Name: ");
+                tempL = Console.ReadLine();
+                if (tempL.Contains(" "))
+                {
+                    Console.WriteLine("ONE NAME ONLY");
+                    goto ask2;
+                }
+                Console.Write("Email: ");
+                tempE = Console.ReadLine();
+                person.Add(new Person(tempF, tempL, int.Parse(tempAge), tempE));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Person> person = new List<Person>();
+            person.Add(new Person("Robert", "Paulson", 35, "rp123@gmail.com"));
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    NEWPERSON(person);
+                    foreach (var v in person)
+                    {
+                        Console.WriteLine($"{v.FirstName} {v.LastName}, age {v.Age}. Email {v.Email}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.Write("Enter 'n' to quit: ");
+                if (Console.ReadLine().ToLower() == "n")
+                    break;
+            }
+        }
+    }
+}
